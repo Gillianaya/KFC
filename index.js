256c38b6e13ba4a482752bac05cb660b26ef9fe2
evelinomaira <eveescobarsuarez@crackthecode.la>
2024-08-16 10:20:26 -0500

    asjxadawdj

--- index.js
+++ index.js
@@ -16,16 +16,45 @@ function lit() {
   var loro = document.getElementById("masa");
   var mensaje = btoa(nose);
   loro.innerHTML = mensaje;
-  console.log(mensaje); 
+  console.log(mensaje);
+  
+  if (nose == ""){
+    Swal.fire({
+      imageUrl: "https://ih1.redbubble.net/image.5426124992.1205/raf,360x360,075,t,fafafa:ca443f4786.jpg",
+      imageHeight: 500,
+      text: "INTELIGENTE escribe algo.",
+      imageAlt: ""
+    }); 
+  } else {
+    //jijijiji
+  }
 }
+
 
 function lete() {
   var sise = document.getElementById("ALA").value;
   var orol = document.getElementById("moso");
   var mensajito = atob(sise);
   orol.innerHTML = mensajito;
-  console.log(mensajito); 
+  console.log(mensajito);
+  
+  if (sise == ""){
+    Swal.fire({
+      imageUrl: "https://ih1.redbubble.net/image.5426124992.1205/raf,360x360,075,t,fafafa:ca443f4786.jpg",
+      imageHeight: 500 ,
+      text: "INTELIGENTE escribe algo.",
+      imageAlt: ""
+    }); 
+    
+  } else {
+    //jijijiji
+  }
 }
+
+
+
+
+
 
 
 
